6b74ba01321832ced63786244e69fc544dfdb134
dragan002 <draganvujic29@gmail.com>
2024-03-28 18:13:33 +0100

    IMprove db

--- OOP/Construct, Static/models/Database.php
+++ OOP/Construct, Static/models/Database.php
@@ -1,18 +1,27 @@
 <?php
 require_once 'dbCredentials.php';
+
 class Database {
     private $conn;
+    const DB_SERVER = 'localhost';
+    const DB_USERNAME =  'root';
+    const DB_PASSWORD = '';
+    const DB_DATABASE = 'web_shop';
 
     public function __construct() {
         try {
-            $dsn = "mysql:host=" . DB_SERVER . ";dbname=" . DB_DATABASE;
+            $dsn = "mysql:host=" . self::DB_SERVER . ";dbname=" . self::DB_DATABASE;
             $options = [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_EMULATE_PREPARES => false,
             ];
-            $this->conn = new PDO($dsn, DB_USERNAME, DB_PASSWORD, $options);
+            $this->conn = new PDO($dsn, self::DB_USERNAME, self::DB_PASSWORD, $options);
         } catch(PDOException $e) {
             die("ERROR: Could not connect. " . $e->getMessage());
         }
     }
 }
+
+$conn = new Database();
+var_dump($conn);
+?>
