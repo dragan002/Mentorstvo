3a15a40952db1036bc3238bb83f00d5a977b1761
dragan002 <draganvujic29@gmail.com>
2024-04-06 23:42:11 +0200

    Better readabillity

--- webshop/index.php
+++ webshop/index.php
@@ -4,8 +4,8 @@ require(__DIR__ . '/../vendor/autoload.php');
 $database = new App\Model\Database\Database();
 $conn = $database->getConnection();
 
-$items = new App\Model\Products\Products();
-$products = $items->getAllProduct();
+$productModel = new App\Model\Products\Products();
+$products = $productModel->getAllProduct();
 
 require_once 'View/includes/header.php';
 ?>
@@ -13,29 +13,32 @@ require_once 'View/includes/header.php';
 <div class="container mt-5">
     <div class="row">
         <div class="col-md-8 offset-md-2">
-            <?php
-                session_start();
+        <?php 
+            session_start(); 
+            if(isset($_SESSION['success_message'])): 
+        ?>
+            <div class="alert alert-success" role="alert">
+                <?= $_SESSION['success_message'] ?>
+            </div>
+        <?php unset($_SESSION['success_message']); ?>
+        <?php endif; ?>
 
-                if(isset($_SESSION['success_message'])) {
-                    echo '<div class="alert alert-success" role="alert">' . $_SESSION['success_message'] . '</div>';
-                    unset($_SESSION['success_message']);
-                }
-            ?>
             <div class="list-group">
-                <?php foreach($products as $product) { ?>
+                <?php foreach($products as $product): ?>
                     <div class="list-group-item mb-3 shadow">
-                        <h3 class="mb-1 "><?= $product['name']; ?></h3>
+                        <h3 class="mb-1"><?= $product['name']; ?></h3>
                         <p class="mb-1"><?= $product['description']; ?></p>
                         <p class="mb-1">Price: <?= $product['price']; ?></p>
-                        <p class="mb-1"><?= ($product['quantity']) == 0 ? "Out of Stock" : "In Stock"; ?></p>
+                        <p class="mb-1"><?= ($product['quantity'] == 0) ? "Out of Stock" : "In Stock"; ?></p>
                         <a href="View/product/product.php?id=<?= $product['id']; ?>" class="btn btn-primary">Details</a>
-                        <button class="btn btn-success " onclick="addToCart(<?= $product['id']; ?>)">Add to Cart</button>
+                        <button class="btn btn-success" onclick="addToCart(<?= $product['id']; ?>)">Add to Cart</button>
                     </div>
-                <?php } ?>
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
 </div>
+
 
 <?php
 require_once 'View/includes/footer.php';
