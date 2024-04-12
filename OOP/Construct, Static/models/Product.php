cce635550c4750cd0043538ac0686055cea972fa
dragan002 <draganvujic29@gmail.com>
2024-04-12 17:01:36 +0200

    PHP OOP Construct, Static - Exercise 2

--- OOP/Construct, Static/models/Product.php
+++ OOP/Construct, Static/models/Product.php
@@ -6,4 +6,12 @@ class Product {
     public $price;
     public $image;
     public $quantity;
+
+    public function __construct($name, $desc, $price, $image, $quantity) {
+        $this->name = $name;
+        $this->desc = $desc;
+        $this->price = $price;
+        $this->image = $image;
+        $this->quantity = $quantity;
+    }
 }
